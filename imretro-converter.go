15af782f486997e73f19dc030ca37ed084664d0d
Spenser Black <spenserblack01@gmail.com>
2022-02-21 09:49:22 -0500

    Add flags

--- imretro-converter.go
+++ imretro-converter.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"os"
+)
+
+var help bool
 
 func main() {
-	fmt.Println("Hello, World!")
+	flag.Parse()
+	if help {
+		printHelp()
+		return
+	}
+
+	source := flag.Arg(0)
+	dest := flag.Arg(1)
+
+	if source == "" {
+		exitOnError(errors.New("source not defined"))
+	} else if dest == "" {
+		exitOnError(errors.New("dest not defined"))
+	}
+}
+
+func init() {
+	flag.BoolVar(&help, "help", false, "print this message and exit")
+}
+
+func printHelp() {
+	fmt.Fprintf(os.Stderr, "usage: imretro-converter [FLAGS] <source> <dest>\n\nFLAGS:\n")
+	flag.PrintDefaults()
+}
+
+func exitOnError(err error) {
+	fmt.Fprintf(os.Stderr, "ERROR: %v\n\n", err)
+	printHelp()
+	os.Exit(1)
 }
